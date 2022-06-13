d003cad9138717a00e0c2eaf599387f411ddc85c
Masked-Idiot <blaze12768@gmail.com>
2022-06-13 18:37:22 -0400

    THE BEST UPDATE

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from os import system as cmd
 from os.path import dirname as path
+from os import listdir as files
 read = open("path.txt", "r")
 cmd("git remote add origin " + read.readlines()[0])
-path = path(____)
+path = path(read.readlines()[1])
 write = open("path.txt", "w")
 write.write("")
 read.close()
