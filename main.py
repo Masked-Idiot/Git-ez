4697d9da6bb1278c436f7655f0abef22fd9ffeeb
Masked-Idiot <blaze12768@gmail.com>
2022-06-13 17:40:59 -0400

    I AM HAVING A MENTAL BREAK-DOWN

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from os import system as cmd
 from os.path import dirname as path
 read = open("path.txt", "r")
 cmd("git remote add origin " + read.readlines()[0])
+path = path(____)
 write = open("path.txt", "w")
 write.write("")
 read.close()
