ca950d6b3f62276008b0d47f71500c21dfb8495e
Masked-Idiot <blaze12768@gmail.com>
2022-06-14 09:00:26 -0400

    cool message

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from os import system as cmd
 from os import listdir as files
 from random import randint as random
+message = ""
 read = open("path.txt", "r")
 messages = open("messages.txt", "r").readlines()
+for i in range(random(0, len(messages))):
+	message = messages[i]
 lines = read.readlines()
 path = lines[1]
 cmd("cd " + path)
 cmd("git remote add origin " + lines[0])
 cmd("git add .")
-cmd("git commit -m " + "\"" + messages[random(0, len(messages))] + "\"")
+cmd("git commit -m " + "\"" + message + "\"")
 write = open("path.txt", "w")
 write.write("")
 read.close()
