a76e6e1179f546f6f59b65ed4e3680f9536085bf
Masked-Idiot <blaze12768@gmail.com>
2022-06-14 08:56:01 -0400

    cool message

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from random import randint as random
 read = open("path.txt", "r")
 messages = open("messages.txt", "r").readlines()
 lines = read.readlines()
-cmd("git remote add origin " + lines[0])
 path = lines[1]
 cmd("cd " + path)
+cmd("git remote add origin " + lines[0])
 cmd("git add .")
 cmd("git commit -m " + "\"" + messages[random(0, len(messages))] + "\"")
 write = open("path.txt", "w")
